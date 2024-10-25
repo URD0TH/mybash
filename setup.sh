de5603ab3a97bfe35577b04cf26db2b531d13b54
URD0TH <urd0th@gmail.com>
2024-10-25 00:20:31 -0300

    fix custom path

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ YELLOW='\033[33m'
 GREEN='\033[32m'
 
 # Check if the home directory and linuxtoolbox folder exist, create them if they don't
-LINUXTOOLBOXDIR="$HOME/linuxtoolbox"
+LINUXTOOLBOXDIR="$HOME/github"
 
 if [ ! -d "$LINUXTOOLBOXDIR" ]; then
     echo "${YELLOW}Creating linuxtoolbox directory: $LINUXTOOLBOXDIR${RC}"
