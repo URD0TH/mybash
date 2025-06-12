f3aab162954790d0290dfa0768ed1dff351a6b59
URD0TH <urd0th@gmail.com>
2025-06-11 22:49:37 -0400

    remove some dependencies

--- setup.sh
+++ setup.sh
@@ -112,7 +112,7 @@ check_environment() {
 }
 
 install_dependencies() {
-    DEPENDENCIES='bash bash-completion tar bat tree multitail fastfetch wget unzip fontconfig trash-cli'
+    DEPENDENCIES='bash bash-completion tar bat tree multitail wget unzip fontconfig'
     if ! command_exists nvim; then
         DEPENDENCIES="${DEPENDENCIES} neovim"
     fi
